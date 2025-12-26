081d8e3f56838ebb198f18a0f6ce988f51e8e340
Ashish Kurzekar <ashishkurzekar.ak@gmail.com>
2025-12-26 18:46:22 +0530

    changes 2612

--- force-app/main/default/lwc/portfolioContact/portfolioContact.js
+++ force-app/main/default/lwc/portfolioContact/portfolioContact.js
@@ -1,5 +1,16 @@
 import { LightningElement } from 'lwc';
+import emailIcon from '@salesforce/resourceUrl/email_icon';
+import linkedinIcon from '@salesforce/resourceUrl/linkedin_icon';
+import githubIcon from '@salesforce/resourceUrl/github_icon';
+import whatsappIcon from '@salesforce/resourceUrl/whatsapp_icon';
 export default class PortfolioContact extends LightningElement {
+
+    emailIcon = emailIcon;
+    linkedinIcon = linkedinIcon;
+    githubIcon = githubIcon;
+    whatsappIcon = whatsappIcon;
+
+    
     get currentYear() {
         return new Date().getFullYear();
     }
